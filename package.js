2a605acb8d12df93e72c4c8b450d3a21a28dbf59
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-02 10:30:17 +0200

    Updates npm-shrinkwrap.json

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'sanjo:karma',
   summary: 'Integrates Karma into Meteor',
-  version: '1.4.3',
+  version: '1.5.0-rc.1',
   git: 'https://github.com/Sanjo/meteor-karma.git'
 })
 
