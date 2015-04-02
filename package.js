73898979652ada09d60aac7cbdd600caf31c85c1
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-02 15:05:07 +0200

    Release of 1.5.0-rc.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'sanjo:karma',
   summary: 'Integrates Karma into Meteor',
-  version: '1.5.0-rc.2',
+  version: '1.5.0-rc.3',
   git: 'https://github.com/Sanjo/meteor-karma.git'
 })
 
